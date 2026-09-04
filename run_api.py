23cfe8db90612ef7ef2f5940d8fd392220b1cc75
Sajed Fallah <fallah.sajed@gmail.com>
2026-09-05 02:04:40 +0330

    feat: enable two-digit signal codes in AutoTrade API runtime

--- run_api.py
+++ run_api.py
@@ -11,6 +11,9 @@ os.environ.setdefault("PUBLIC_CHANNEL_URL", "https://t.me")
 from app.telegram_topic_routing import install_free_topic_routing
 install_free_topic_routing()
 
+from app.signal_code_runtime import install_two_digit_signal_codes
+install_two_digit_signal_codes()
+
 from app.autotrade.live_event_runtime import install_live_snapshot_event_bridge
 install_live_snapshot_event_bridge()
 
