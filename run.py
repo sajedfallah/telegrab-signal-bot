f518cb42bd88aa0f09836df756ce2486362b5584
Sajed Fallah <fallah.sajed@gmail.com>
2026-09-05 02:15:58 +0330

    feat: install admin bot invite shortcut

--- run.py
+++ run.py
@@ -36,6 +36,7 @@ from app.autotrade_user_runtime import install_autotrade_user_experience
 from app.autotrade_cleanup_runtime import install_autotrade_durable_cleanup
 from app.customer_experience import install_customer_experience
 from app.customer_menu_runtime import install_customer_menu_runtime
+from app.admin_referral_invite_runtime import install as install_admin_referral_invite
 from app.topic_admin import router as topic_admin_router
 from app.content.runner import main as content_main
 
@@ -57,6 +58,7 @@ install_autotrade_durable_cleanup(main_module)
 
 install_customer_experience(main_module)
 install_customer_menu_runtime(main_module)
+install_admin_referral_invite(main_module)
 
 
 def _restrict_core_catchall_to_private() -> None:
