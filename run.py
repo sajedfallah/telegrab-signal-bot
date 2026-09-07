afd5d62f05c63c4011e7aedf829f710ef2d01e57
Sajed Fallah <fallah.sajed@gmail.com>
2026-09-07 23:37:15 +0330

    feat: wire performance marketing engine into production runtime

--- run.py
+++ run.py
@@ -31,6 +31,7 @@ from app.services.report_runtime import install as install_report_runtime
 from app.services.market_brief_service import install as install_market_brief_runtime
 from app.services.market_public_channel_runtime import install as install_market_public_channel_runtime
 from app.services.public_content_runtime import install as install_public_content_runtime
+from app.services.performance_marketing_runtime import install as install_performance_marketing_runtime
 from app.services.market_content_route_runtime import install as install_market_content_route_runtime
 from app.services.open_access_runtime import install as install_open_access_runtime
 from app.portal_runtime import install_nexus_hub
@@ -57,6 +58,7 @@ install_report_runtime(main_module)
 install_market_brief_runtime(main_module)
 install_market_public_channel_runtime(main_module)
 install_public_content_runtime(main_module)
+install_performance_marketing_runtime(main_module)
 install_market_content_route_runtime(main_module)
 install_open_access_runtime(main_module)
 install_nexus_hub(main_module)
