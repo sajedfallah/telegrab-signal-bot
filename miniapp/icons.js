de3b118c28da3107439e0fb434e2e313e18e7ee2
Sajed Fallah <fallah.sajed@gmail.com>
2026-09-10 10:48:07 +0330

    feat: expand local SVG icon system for polished Mini App

--- miniapp/icons.js
+++ miniapp/icons.js
@@ -7,6 +7,8 @@
     account: '<circle cx="12" cy="8" r="4"/><path d="M4.5 20c1.2-4 3.7-6 7.5-6s6.3 2 7.5 6"/>',
     lock: '<rect x="5" y="10" width="14" height="10" rx="2"/><path d="M8 10V7a4 4 0 0 1 8 0v3"/>',
     arrowUpRight: '<path d="M7 17 17 7"/><path d="M8 7h9v9"/>',
+    arrowLeft: '<path d="m15 18-6-6 6-6"/>',
+    arrowRight: '<path d="m9 18 6-6-6-6"/>',
     chart: '<path d="M4 20V10"/><path d="M10 20V4"/><path d="M16 20v-7"/><path d="M22 20H2"/>',
     community: '<circle cx="9" cy="8" r="3"/><circle cx="17" cy="9" r="2.5"/><path d="M3.5 19c.8-3.7 2.6-5.5 5.5-5.5s4.7 1.8 5.5 5.5"/><path d="M14 15c1-.9 2-1.3 3.2-1.3 2.2 0 3.6 1.4 4.3 4.3"/>',
     shield: '<path d="M12 3 5 6v5c0 4.6 2.8 8 7 10 4.2-2 7-5.4 7-10V6z"/><path d="m9 12 2 2 4-5"/>',
@@ -15,6 +17,12 @@
     check: '<path d="m5 12 4 4L19 6"/>',
     close: '<path d="M6 6l12 12M18 6 6 18"/>',
     clock: '<circle cx="12" cy="12" r="9"/><path d="M12 7v5l3 2"/>',
+    bell: '<path d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 9h18c0-2-3-2-3-9"/><path d="M10 21h4"/>',
+    info: '<circle cx="12" cy="12" r="9"/><path d="M12 11v5"/><path d="M12 8h.01"/>',
+    alert: '<path d="M12 3 2.8 20h18.4z"/><path d="M12 9v4"/><path d="M12 17h.01"/>',
+    book: '<path d="M4 5.5A2.5 2.5 0 0 1 6.5 3H11v16H6.5A2.5 2.5 0 0 0 4 21.5z"/><path d="M20 5.5A2.5 2.5 0 0 0 17.5 3H13v16h4.5A2.5 2.5 0 0 1 20 21.5z"/>',
+    copy: '<rect x="8" y="8" width="11" height="11" rx="2"/><path d="M16 8V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h3"/>',
+    retry: '<path d="M20 6v5h-5"/><path d="M19 11a8 8 0 1 0 1 5"/>',
     sparkle: '<path d="M12 3l1.4 3.6L17 8l-3.6 1.4L12 13l-1.4-3.6L7 8l3.6-1.4z"/><path d="M19 14l.8 2.2L22 17l-2.2.8L19 20l-.8-2.2L16 17l2.2-.8z"/>',
   };
 
