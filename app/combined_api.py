3d7f8cdca72efb8c9c9aa96a7f4bb291c8a8d819
Sajed Fallah <fallah.sajed@gmail.com>
2026-09-15 01:58:26 +0330

    feat: mount Provider branding API

--- app/combined_api.py
+++ app/combined_api.py
@@ -6,6 +6,7 @@ from fastapi.staticfiles import StaticFiles
 
 from .autotrade.api import app
 from .miniapp_api import router as miniapp_router
+from .provider_branding_api import router as provider_branding_router
 from .provider_lifecycle_api import router as provider_lifecycle_router
 from .provider_panel_api import router as provider_panel_router
 from .provider_reconciliation_api import router as provider_reconciliation_router
@@ -27,6 +28,7 @@ app.include_router(provider_reconciliation_router)
 app.include_router(provider_subscribers_router)
 app.include_router(provider_revenue_router)
 app.include_router(provider_reports_router)
+app.include_router(provider_branding_router)
 
 MINIAPP_DIR = Path(__file__).resolve().parent.parent / "miniapp"
 if MINIAPP_DIR.is_dir():
