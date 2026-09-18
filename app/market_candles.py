d88c28e05153f9dfdedba07b20976f03b3cf458c
Sajed Fallah <fallah.sajed@gmail.com>
2026-09-18 19:26:21 +0330

    fix: support all Admin Mini App chart timeframes

--- app/market_candles.py
+++ app/market_candles.py
@@ -17,10 +17,12 @@ _ALLOWED_TIMEFRAMES = {
     "1M": "M1", "M1": "M1",
     "5M": "M5", "M5": "M5",
     "15M": "M15", "M15": "M15",
+    "30M": "M30", "M30": "M30",
     "1H": "H1", "H1": "H1",
+    "4H": "H4", "H4": "H4",
     "1D": "D1", "D1": "D1",
 }
-_PUBLIC_TIMEFRAMES = {"M1": "1m", "M5": "5m", "M15": "15m", "H1": "1h", "D1": "1D"}
+_PUBLIC_TIMEFRAMES = {"M1": "1m", "M5": "5m", "M15": "15m", "M30": "30m", "H1": "1h", "H4": "4h", "D1": "1D"}
 _MAX_SERIES = 50
 _MAX_CANDLES_PER_SERIES = 500
 _RETAIN_PER_SERIES = 1500
